5d0515d52ef6bcf72736d2fc7a00b0e763b93c43
Nando Davis <nandodavis@gmail.com>
2019-06-30 19:27:07 +0200

    Create server

--- client/client.go
+++ client/client.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/binary"
+	"encoding/json"
 	"fmt"
 	"github.com/gordonklaus/portaudio"
 	"io/ioutil"
@@ -10,22 +11,40 @@ import (
 	"time"
 )
 
+type Settings struct {
+	SampleRate float64
+	Buffer     int
+	Channels   int
+}
+
+const HOST = "http://localhost:5656"
+
 func main() {
-	const sampleRate = 44100
+	resp, err := http.Get(HOST + "/settings")
+	chk(err)
+	body, _ := ioutil.ReadAll(resp.Body)
+	var settings Settings
+	json.Unmarshal(body, &settings)
 	portaudio.Initialize()
 	defer portaudio.Terminate()
-	buffer := make([]float32, 512)
+	buffer := make([]float32, settings.Buffer)
 
-	stream, err := portaudio.OpenDefaultStream(0, 1, sampleRate, len(buffer), func(out []float32) {
-		resp, err := http.Get("http://localhost:5656/audio")
-		chk(err)
-		body, _ := ioutil.ReadAll(resp.Body)
-		responseReader := bytes.NewReader(body)
-		binary.Read(responseReader, binary.BigEndian, &buffer)
+	stream, err := portaudio.OpenDefaultStream(0, 1, settings.SampleRate, len(buffer), func(out []float32) {
+		buffers := make([][]float32, settings.Channels)
+		for i := 0; i < settings.Channels; i++ {
+			buffers[i] = make([]float32, settings.Buffer)
+			resp, err := http.Get(fmt.Sprintf("%s/audio_channel%d", HOST, i))
+			chk(err)
+
+			body, _ := ioutil.ReadAll(resp.Body)
+			responseReader := bytes.NewReader(body)
+			binary.Read(responseReader, binary.BigEndian, &buffers[i])
+		}
 		for i := range out {
-			out[i] = buffer[i]
+			out[i] = mix(buffers[0][i], buffers[1][i])
 		}
 	})
+
 	chk(err)
 	chk(stream.Start())
 	time.Sleep(time.Second * 40)
@@ -36,6 +55,15 @@ func main() {
 		fmt.Println(err)
 	}
 
+}
+
+func mix(a float32, b float32) float32 {
+	if a < 0 && b < 0 {
+		return (a + b) - ((a * b) / -32767)
+	} else if a > 0 && b > 0 {
+		return (a + b) - ((a * b) / -32767)
+	}
+	return a + b
 }
 
 func chk(err error) {
--- server/server.go
+++ server/server.go
@@ -2,43 +2,79 @@ package main
 
 import (
 	"encoding/binary"
+	"encoding/json"
+	"fmt"
 	"github.com/gordonklaus/portaudio"
 	"net/http"
 )
 
 const sampleRate = 44100
 
-func main() {
+type Settings struct {
+	SampleRate float64
+	Buffer     int
+	Channels   int
+}
 
+func main() {
 	portaudio.Initialize()
 	defer portaudio.Terminate()
-	buffer := make([]float32, 512)
-	stream, err := portaudio.OpenDefaultStream(1, 0, sampleRate, len(buffer), func(in []float32) {
-		for i := range buffer {
-			buffer[i] = in[i]
-		}
+
+	var settings Settings
+	settings.SampleRate = sampleRate
+	device, err := portaudio.DefaultInputDevice()
+	settings.Channels = device.MaxInputChannels
+	settings.Buffer = 44100 * 20
+
+	buffers := make([][]float32, settings.Channels)
+	for i := range buffers {
+		buffers[i] = make([]float32, settings.Buffer)
+	}
+	stream, err := portaudio.OpenDefaultStream(settings.Channels, 0, settings.SampleRate, settings.Buffer, func(in []float32) {
+		go func(in []float32) {
+			count := 0
+			index := 0
+			for j := range in {
+				buffers[count][index] = in[j]
+				if count == settings.Channels-1 {
+					count = 0
+					index++
+				} else {
+					count++
+				}
+			}
+		}(in)
 	})
 	chk(err)
-	chk(stream.Start())
+	err = stream.Start()
+	chk(err)
 	defer stream.Close()
 
-	http.HandleFunc("/audio", func(w http.ResponseWriter, r *http.Request) {
-		flusher, ok := w.(http.Flusher)
-		if !ok {
-			panic("expected http.ResponseWriter to be an http.Flusher")
-		}
-
-		w.Header().Set("Connection", "Keep-Alive")
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("X-Content-Type-Options", "nosniff")
-		w.Header().Set("Transfer-Encoding", "chunked")
-		w.Header().Set("Content-Type", "audio/wave")
-		for true {
-			binary.Write(w, binary.BigEndian, &buffer)
-			flusher.Flush() // Trigger "chunked" encoding and send a chunk...
-			return
-		}
+	http.HandleFunc("/settings", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		js, _ := json.Marshal(settings)
+		w.Write(js)
 	})
+
+	for i := range buffers {
+		http.HandleFunc(fmt.Sprintf("/audio_channel%d", i), func(w http.ResponseWriter, r *http.Request) {
+			flusher, ok := w.(http.Flusher)
+			if !ok {
+				panic("expected http.ResponseWriter to be an http.Flusher")
+			}
+
+			w.Header().Set("Connection", "Keep-Alive")
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+			w.Header().Set("X-Content-Type-Options", "nosniff")
+			w.Header().Set("Transfer-Encoding", "chunked")
+			w.Header().Set("Content-Type", "audio/wave")
+			for true {
+				binary.Write(w, binary.BigEndian, &buffers[i])
+				flusher.Flush() // Trigger "chunked" encoding and send a chunk...
+				return
+			}
+		})
+	}
 
 	http.ListenAndServe(":5656", nil)
 }
